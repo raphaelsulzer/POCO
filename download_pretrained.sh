2c4a5e3b704e816476cfc8be56aa88b122bcbb15
raphaelsulzer <raphael.sulzer.1@gmail.com>
2022-05-11 18:59:51 +0200

    fixed typo in download_pretrained.sh

--- download_pretrained.sh
+++ download_pretrained.sh
@@ -3,9 +3,9 @@
 echo "Downloading pretrained models..."
 
 if gdown 1g6KNv_lhnxkyugA5D6__6LyVA7KPUk9k; then
-  unzip convonet.zip
-  mv convonet out
-  rm convonet.zip
+  unzip poco.zip
+  mv poco out
+  rm poco.zip
   echo "Done!"
 else
   echo "Please install gdown with 'conda install -c conda-forge gdown'"
